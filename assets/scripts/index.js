b54fc93205f0ae126bbd5fb393b2c29a7ab60524
lydivetteroldan <lydivette.roldan@gmail.com>
2018-05-07 17:25:45 -0400

    Add event handlers function for `todos`

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,12 @@
 'use strict'
 const authEvents = require('./auth/events')
 const authUi = require('./auth/ui')
+const todosEvents = require('./todos/events')
 
 authUi.unauthorized()
 authUi.showSignIn()
 
 $(() => {
   authEvents.eventHandlers()
+  todosEvents.eventHandlers()
 })
